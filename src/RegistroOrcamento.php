e59b35f24848724eeda1cc0f093c770a5e82e36e
Alexandre Moretti <alemoretti10@gmail.com>
2022-03-28 23:35:11 -0300

    Change class to use http adapter

--- src/RegistroOrcamento.php
+++ src/RegistroOrcamento.php
@@ -2,10 +2,25 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\Http\HttpAdapter;
+
 class RegistroOrcamento
 {
+    private HttpAdapter $http;
+
+    public function __construct(HttpAdapter $http)
+    {
+        $this->http = $http;
+    }
+
     public function registrar(Orcamento $orcamento): void
     {
-
+        $this->http->post(
+            'http://api.registrar.orcamento',
+            [
+                'valor' => $orcamento->valor,
+                'quantidadeItens' => $orcamento->quantidadeItens
+            ]
+        );
     }
 }
